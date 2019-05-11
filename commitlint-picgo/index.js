860a3273e82786193c8c5d2d16308a890e920f53
PiEgg <marksz@teamsz.xyz>
2019-05-11 11:16:47 +0800

    :bug: Fix(lint): subject case can't be sentence-case

--- commitlint-picgo/index.js
+++ commitlint-picgo/index.js
@@ -13,8 +13,8 @@ module.exports = {
     ],
     'subject-case': [
       2,
-      'always',
-      ['lower-case', 'upper-case']
+      'never',
+      ['sentence-case']
     ],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
